4377ec6808747bd9aeafd9246f26e703d523e22c
ming <18120580001@163.com>
2024-04-03 22:13:13 +0800

    feat:增加entity 和登录页面

--- src/main/java/com/ming/controller/admin/AuthController.java
+++ src/main/java/com/ming/controller/admin/AuthController.java
@@ -1,6 +1,7 @@
 package com.ming.controller.admin;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +11,10 @@ public class AuthController {
 
     @RequestMapping("/login")
     public String login(HttpServletRequest request) {
-        request.getSession().setAttribute("user","info");
+        request.getSession().setAttribute("user", "info");
+        if (request.getMethod().equals(HttpMethod.POST.name())) {
+            return "admin/index";
+        }
         return "admin/login";
     }
 
@@ -19,7 +23,6 @@ public class AuthController {
         request.getSession().removeAttribute("user");
         return "admin/login";
     }
-
 
 
 }
