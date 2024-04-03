b2467b7401e73ea8754f13b42a1f921ab380d7f1
ming <18120580001@163.com>
2024-04-03 22:42:52 +0800

    feat:增加登录流程

--- src/main/java/com/ming/controller/admin/AuthController.java
+++ src/main/java/com/ming/controller/admin/AuthController.java
@@ -1,21 +1,29 @@
 package com.ming.controller.admin;
 
+import com.ming.core.dto.JsonResult;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/admin")
 @Controller
 public class AuthController {
 
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public String login(HttpServletRequest request) {
-        request.getSession().setAttribute("user", "info");
-        if (request.getMethod().equals(HttpMethod.POST.name())) {
-            return "admin/index";
-        }
         return "admin/login";
+    }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public JsonResult<String> login1(HttpServletRequest request) {
+        if ("ming".equals(request.getParameter("username"))) {
+            return JsonResult.ok("登录成功");
+        }
+        return JsonResult.error(403, "用户名或密码错误");
     }
 
     @RequestMapping("/logout")
@@ -24,5 +32,9 @@ public class AuthController {
         return "admin/login";
     }
 
+    @RequestMapping(value = {"/index", "/index.html"})
+    public String index() {
+        return "admin/index";
+    }
 
 }
---
+++ src/main/java/com/ming/core/dto/JsonResult.java
@@ -0,0 +1,21 @@
+package com.ming.core.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class JsonResult<T> {
+    private int code;
+    private T data;
+    private String msg;
+
+    public static <T> JsonResult<T> ok(T data) {
+        return JsonResult.<T>builder().code(0).data(data).build();
+    }
+
+    public static <T> JsonResult<T> error(int code, String msg) {
+        return JsonResult.<T>builder().code(code).msg(msg).build();
+    }
+
+}
