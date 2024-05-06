3b6b204ff87b54fc37bce6a7027626b9716484fd
ming <18120580001@163.com>
2024-05-06 12:00:23 +0800

    feat: 定义上传服务接口

--- src/main/java/com/ming/controller/UploadController.java
+++ src/main/java/com/ming/controller/UploadController.java
@@ -1,6 +1,11 @@
 package com.ming.controller;
 
+import com.ming.core.dto.UploadResultDTO;
+import com.ming.service.upload.UploadService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 上传服务
@@ -10,5 +15,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @date 2024-05-06 10:16:29
  */
 @RequestMapping("/upload")
+@RequiredArgsConstructor
 public class UploadController {
+
+    private final UploadService uploadService;
+
+    @RequestMapping
+    public UploadResultDTO upload(@RequestParam("file") MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            throw new RuntimeException("上传文件不能为空");
+        }
+        return uploadService.upload(file);
+    }
+
+    @RequestMapping("/get")
+    public UploadResultDTO get(Long id) {
+        return uploadService.findById(id);
+    }
+
 }
--- src/main/java/com/ming/service/upload/UploadService.java
+++ src/main/java/com/ming/service/upload/UploadService.java
@@ -1,10 +1,13 @@
 package com.ming.service.upload;
 
 import com.ming.core.dto.UploadResultDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+@Service
 public class UploadService {
 
-    public UploadResultDTO upload() {
+    public UploadResultDTO upload(MultipartFile file) {
         return null;
     }
 
