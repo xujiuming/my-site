bdfb31e51f96407bd7aa9e9374a22e744969bbd2
ming <18120580001@163.com>
2024-05-07 20:15:22 +0800

    feat: 初始化hexo格式数据

--- src/test/java/com/ming/ReadHexoBlogTest.java
+++ src/test/java/com/ming/ReadHexoBlogTest.java
@@ -7,6 +7,7 @@ import com.ming.core.utils.SignatureUtils;
 import com.ming.service.MarkdownParseService;
 import com.ming.service.entity.ArticleEntityService;
 import lombok.Data;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,19 +31,51 @@ public class ReadHexoBlogTest {
     @Autowired
     private ArticleEntityService articleEntityService;
 
+    /**
+     * 递归列出目录下所有文件（包括子目录）
+     *
+     * @param dir 当前处理的目录
+     * @return 所有文件的列表
+     */
+    public static List<File> listFilesRecursively(File dir) {
+        List<File> files = new ArrayList<>();
+
+        // 获取当前目录下的所有文件和子目录
+        File[] entries = dir.listFiles();
+        if (entries != null) {
+            for (File entry : entries) {
+                // 如果是文件，直接添加到列表
+                if (entry.isFile()) {
+                    files.add(entry);
+                } else if (entry.isDirectory()) {
+                    // 如果是目录，递归处理
+                    files.addAll(listFilesRecursively(entry));
+                }
+            }
+        }
+        return files;
+    }
+
     @Test
     public void read() throws IOException {
-        //String rootPath = "/home/ming/IdeaProjects/blog/source/_posts";
-        String filePath = "/home/ming/IdeaProjects/blog/source/_posts/java/disruptor使用笔记.md";
+        String rootPath = "/home/ming/IdeaProjects/blog/source/_posts";
 
+        // 获取目录对象
+        File directory = new File(rootPath);
 
-        writeToDb(filePath);
+        // 递归列出所有文件
+        List<File> allFiles = listFilesRecursively(directory);
+
+        allFiles.stream()
+                .filter(f -> f.getName().endsWith(".md"))
+                .forEach(this::writeToDb);
 
     }
 
-    private void writeToDb(String filePath) throws IOException {
+    @SneakyThrows
+    private void writeToDb(File file) {
         //读取文件
-        String allContentString = Files.readString(Path.of(filePath));
+        String allContentString = Files.readString(file.toPath());
         String[] splitStringArr = allContentString.split("---");
         //0 是空数据
         String headtString = splitStringArr[1];
