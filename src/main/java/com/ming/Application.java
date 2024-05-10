245ab9be602e076a09c758e38070cc870f53aa14
ming <18120580001@163.com>
2024-05-10 12:27:29 +0800

    feat: 调整log

--- src/main/java/com/ming/Application.java
+++ src/main/java/com/ming/Application.java
@@ -1,11 +1,13 @@
 package com.ming;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableRedisHttpSession
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
